f1db32f4921eeb2fb72b697fa7bdbae8da142e93
paclengratha <mr.lengratha@gmail.com>
2019-07-04 11:54:20 +0700

    dfds

---
+++ database/migrations/2019_06_18_040018_create_project_lists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_lists', function (Blueprint $table) {
+            $table->bigIncrements('list_id');
+            $table->bigInteger('project_id');
+            $table->string('list_title');
+            $table->bigInteger('created_by');
+            $table->bigInteger('modified_by');
+            $table->bigInteger('deleted_by');
+            $table->datetime('deleted_at');
+            $table->smallInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_lists');
+    }
+}
