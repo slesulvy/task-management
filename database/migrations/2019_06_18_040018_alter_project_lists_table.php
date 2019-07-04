3b53f4a884bb058a94b27a6e1ef9100106c2e3ee
paclengratha <mr.lengratha@gmail.com>
2019-07-04 14:36:11 +0700

    Add nullable to table project+list

---
+++ database/migrations/2019_06_18_040018_alter_project_lists_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterProjectListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /*Schema::create('project_lists', function (Blueprint $table) {
+            $table->bigIncrements('list_id');
+            $table->bigInteger('project_id');
+            $table->string('list_title');
+            $table->bigInteger('created_by');
+            $table->bigInteger('modified_by');
+            $table->bigInteger('deleted_by');
+            $table->datetime('deleted_at');
+            $table->smallInteger('status')->default(0);
+            $table->timestamps();
+        });*/
+
+        Schema::table('project_lists', function (Blueprint $table) {
+            $table->integer('created_by')->nullable();
+            $table->integer('deleted_by')->nullable();
+            $table->integer('modified_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_lists');
+    }
+}
